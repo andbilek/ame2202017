f70084edb1633c5fb1b567d019ac124d29797841
tejaswigowda <tejaswi@foxyninjastudios.com>
2017-10-11 11:11:15 -0700

    in class

--- dialogLib/dialog.js
+++ dialogLib/dialog.js
@@ -1,7 +1,15 @@
 var dialogLib = {
   alert:{
-    show: function(msg){
-    
+    show: function(msg, label){
+      label = label || "OK";
+      var outS = "";
+      outS += "<div class='wrapper'>" +
+          "<div class='frame'>" +
+            "<h1>" + msg + "</h1>"+
+            "<button onclick='dialogLib.alert.hide()'>" + label + "</button>"+
+          "</div>"+
+        "</div>";
+      $("body").append(outS);
     },
     hide: function(){
 
