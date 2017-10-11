0e21f21f6d083e4b15e7b426c103373e3b0bb9e8
tejaswigowda <tejaswi@foxyninjastudios.com>
2017-10-11 11:42:16 -0700

    in class

--- dialogLib/dialog.js
+++ dialogLib/dialog.js
@@ -14,5 +14,37 @@ var dialogLib = {
     hide: function(){
       $(".wrapper").remove();
     }
+  },
+
+  confirm:{
+    _yesClicked: function(){
+      _yCB();
+      dialogLib.confirm.hide();  
+    },
+    _noClicked: function(){
+      _nCB();
+      dialogLib.confirm.hide();  
+    },
+    _yCB: function(){},
+    _nCB: function(){},
+    show: function(msg, yesLabel, noLabel, yesCB, noCB){
+      yesLabel = yesLabel || "OK";
+      noLabel = noLabel || "Cancel";
+      _yCB = yesCB = yesCB || function(){};
+      _nCB = noCB = noCB || function(){};
+
+      var outS = "";
+      outS += "<div class='wrapper'>" +
+          "<div class='frame'>" +
+            "<h1>" + msg + "</h1>"+
+            "<button onclick='dialogLib.confirm._noClicked()'>" + noLabel + "</button>"+
+            "<button onclick='dialogLib.confirm._yesClicked()'>" + yesLabel + "</button>"+
+          "</div>"+
+        "</div>";
+      $("body").append(outS);
+    },
+    hide: function(){
+      $(".wrapper").remove();
+    }
   }
 }
