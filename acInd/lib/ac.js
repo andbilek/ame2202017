816fd5141c81932960d9923d6e93b72c413e377b
student <student@dc-09.mobile.asu.edu>
2017-11-27 11:34:14 -0700

    in class

---
+++ acInd/lib/ac.js
@@ -0,0 +1,13 @@
+var acInd = {
+	show:function(){
+        var outS = "";
+        outS += "<div class='wrapper'>" +
+            "<div class='spinner'>" +
+            "</div>"+
+          "</div>";
+        $("body").append(outS);
+	},
+	hide:function(){
+        $(".wrapper").remove();
+	}
+}
